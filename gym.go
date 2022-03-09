5257aa92966bb020e6120b5bf64a9f69bc0a309e
lylecantcode <lylejack75@gmail.com>
2022-03-09 01:40:52 +0000

    Not exporting properly into SQL

--- gym.go
+++ gym.go
@@ -6,35 +6,56 @@ import (
 	"time"
 	"os"
 	_"github.com/mattn/go-sqlite3"
+	//"bufio"
 )
 
 func main() {
-	var weight int
-	var unit, date string
-	date = time.Now().Format("02-01-2006")
-	fmt.Println("What weight did you lift? e.g. 5 kg, 10 lbs")
-	fmt.Scanf("%d %s", &weight, &unit)
-	if !((weight > 0 && weight < 1000) && (unit == "kg" || unit == "lbs")){
-		fmt.Println("Weight must be an integer, units must be \"kg\" or \"lbs\".")
-		os.Exit(1) 
-	}
 	database, _ :=
 		sql.Open("sqlite3", "./gym.db")
 	statement, _ :=
-		database.Prepare("CREATE TABLE IF NOT EXISTS weights (id INTEGER PRIMARY KEY, weight INT NOT NULL, units TEXT NOT NULL, date TEXT NOT NULL)")
+		database.Prepare("CREATE TABLE IF NOT EXISTS weights (id INTEGER PRIMARY KEY, weight INT NOT NULL, units TEXT NOT NULL, sets INT NOT NULL DEFAULT 3, date TEXT NOT NULL)")
 	statement.Exec()
-	statement, _ =
-		database.Prepare("INSERT INTO weights (weight, units, date) VALUES (?,?,?)")
-	statement.Exec(weight, unit, date)
-	rows, _ :=
-		database.Query("SELECT * FROM weights")
-	
-	var id int 
-	//var units, date string
-	for rows.Next() {
-		rows.Scan(&id, &weight, &unit, &date)
-		fmt.Println(id, ":", weight, unit, "on", date)
+
+	var weight, sets int
+	var unit, date, formatting string
+	date = time.Now().Format("02-01-2006")
+
+	fmt.Println("How many different exercises have you done?")
+	exercisesDone := 1
+	fmt.Scanf("%d", &exercisesDone)
+	if exercisesDone < 1{
+		fmt.Println("Must have done at least 1 exercise.")
+		os.Exit(1)
 	}
 
+	for i := 0; i < exercisesDone; i++{
+		fmt.Println("What weight and how many sets did you do for exercise", i+1, "? e.g. 5 kg x 3, 40 lbs x 1",)
+		fmt.Scanf("%d %s %s %d", &weight, &unit, &formatting, &sets)
+		if !((weight > 0 && weight < 1000) && (sets > 0 && sets < 1000) && (unit == "kg" || unit == "lbs")){
+			fmt.Println("Weight must be an integer, units must be \"kg\" or \"lbs\".")
+			os.Exit(2) 
+		addToDatabase(weight, sets, unit, date)
+		}
+	}
+
+	
+	var id int
+	rows, _ :=
+		database.Query("SELECT * FROM weights")
+	for rows.Next() {
+		rows.Scan(&id, &weight, &unit, &sets, &date)
+		fmt.Println(id, ":", weight, unit, "x", sets, "on", date)
+	}
+	for rows.Next() {
+		rows.Scan(&id, &weight, &unit, &sets, &date)
+		fmt.Println(id, ":", weight, unit, "x", sets, "on", date)
+	}
 }
 
+func addToDatabase(weight, sets int, unit, date string){
+	database, _ :=
+		sql.Open("sqlite3", "./gym.db")
+	statement, _ :=
+			database.Prepare("INSERT INTO weights (weight, units, sets, date) VALUES (?, ?, ?, ?)")
+	statement.Exec(weight, unit, sets, date)
+} 
