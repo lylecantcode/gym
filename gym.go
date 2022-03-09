e681aaf73a4bd6f22421563c8c17815d190bfac0
lylecantcode <lylejack75@gmail.com>
2022-03-09 13:46:19 +0000

    using structs for passing the data

--- gym.go
+++ gym.go
@@ -9,12 +9,12 @@ import (
 	"strings"
 )
 
-/*type weightLifting struct{
+type weightLifting struct{
 	exercise string
 	weight int
 	unit string
 	sets int
-}*/
+}
 
 func main() {
 	database, _ :=
@@ -25,8 +25,7 @@ func main() {
 
 	exercises := []string{"bench", "curls", "dips", "farmers-walks", "good-mornings", "overhead-press", "pullups", "rows", "squats", "tricep-dips", "tricep-ext"}
 	unitOptions := []string{"bodyweight", "kg", "kgs", "lb", "lbs"}
-	var weight, sets int
-	var unit, date, exercise, formatting string
+	var date, formatting string
 	date = time.Now().Format("02-01-2006")
 
 	fmt.Println("How many different exercises have you done?")
@@ -38,33 +37,35 @@ func main() {
 	}
 	var check bool
 	for i := 0; i < exercisesDone; i++{
+		wl := weightLifting {"exercise", 0, "units", 0}
 		fmt.Println("What was exercise", i+1, "? Please choose from the following:\n", exercises) 
-		fmt.Scanf("%s", &exercise)
-		exercise = strings.ToLower(exercise)
-		check, exercise = validValue(exercise, exercises)
+		fmt.Scanf("%s", &wl.exercise)
+		wl.exercise = strings.ToLower(wl.exercise)
+		check, wl.exercise = validValue(wl.exercise, exercises)
 		if !check {
 			fmt.Println("Invalid exercise.")
 			os.Exit(2)
 		}
 
-		fmt.Println("Please input weight for", exercise, "in the format:  5 kg x 3 || 40 lbs x 1")
-		fmt.Scanf("%d %s %s %d", &weight, &unit, &formatting, &sets)
-		unit = strings.ToLower(unit)
-		check, unit = validValue(unit, unitOptions)
+		fmt.Println("Please input weight for", wl.exercise, "in the format:  5 kg x 3 || 40 lbs x 1")
+		fmt.Scanf("%d %s %s %d", &wl.weight, &wl.unit, &formatting, &wl.sets)
+		wl.unit = strings.ToLower(wl.unit)
+		check, wl.unit = validValue(wl.unit, unitOptions)
 		if !check {
 			fmt.Println("Invalid units, please use one of:", unitOptions)
 			os.Exit(3)
 		}
 
-		if !((weight > 0 && weight < 1000) && (sets > 0 && sets < 1000)){
+		if !((wl.weight > 0 && wl.weight < 1000) && (wl.sets > 0 && wl.sets < 1000)){
 			fmt.Println("Weight must be a positive integer.")
 			os.Exit(4)
 		}
-		addToDatabase(weight, sets, exercise, unit, date, database)
+		wl.addToDatabase(date, database)
 	}
 
 	
-	var id int
+	var id, weight, sets int
+	var exercise, unit string
 	rows, _ :=
 		database.Query("SELECT * FROM weights")
 	for rows.Next() {
@@ -73,10 +74,11 @@ func main() {
 	}
 }
 
-func addToDatabase(weight, sets int, exercise, unit, date string, database *sql.DB){
+
+func (wl weightLifting) addToDatabase(date string, database *sql.DB){
 	statement, _ :=
 			database.Prepare("INSERT INTO weights (exercise, weight, units, sets, date) VALUES (?, ?, ?, ?, ?)")
-	statement.Exec(exercise, weight, unit, sets, date)
+	statement.Exec(wl.exercise, wl.weight, wl.unit, wl.sets, date)
 } 
 
 
